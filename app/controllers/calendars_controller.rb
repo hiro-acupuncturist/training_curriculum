cd356ff274f64aebe2aa318ba666bab72cea671b
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-05-19 16:34:05 +0900

    Issue4の解決

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,8 +15,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
-  end
+    params.require(:plan).permit(:date, :plan)
+  end 
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
